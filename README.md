eff7f0c22e2703ce9062b0a81127cbf1b076f726
Romy Ilano <romy@snowyla.com>
2013-01-30 09:13:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Temperature App
+
+Part of the iOS Study Group at San Francisco's Women Who Code Meetup Group.
+
+
---
+++ Temperature/Temperature/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Temperature
+//
+//  Created by Romy Ilano on 1/30/13.
+//  Copyright (c) 2013 Snowyla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
